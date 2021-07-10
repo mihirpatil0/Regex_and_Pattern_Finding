b57fe86a2227bdecbd8114806b44ebdcd00dd430
mihirpatil0 <mihirrpatil@gmail.com>
2021-07-11 03:32:15 +0530

    [mihir] Added: checking if entered password is contains 8 characters of not.

--- src/com/bridgelab/userregistrationproblem/UserDetails.java
+++ src/com/bridgelab/userregistrationproblem/UserDetails.java
@@ -12,6 +12,7 @@ public class UserDetails
 	private String lastName;
 	private String emailId;
 	private String mobileNumber;
+	private String password;
 
 	public String getFirstName()
 	{
@@ -52,11 +53,21 @@ public class UserDetails
 	{
 		this.mobileNumber = mobileNumber;
 	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
 
 	@Override
 	public String toString()
 	{
-		return "User-Details : [ First-Name = " + firstName + ", Last-Name = " + lastName + ", Email-ID = " + emailId + ", Mobile-Number = " + mobileNumber + " ]";
+		return "User-Details : [ First-Name = " + firstName + ", Last-Name = " + lastName + ", Email-ID = " + emailId + ", Mobile-Number = " + mobileNumber + ", Password = " + password + " ]";
 	}
 	
 }
